8af9aaebe0de34ea7fdd6b2f2f9bb6123348e696
prasadsunny1 <prasadsunny1@gmail.com>
2019-11-27 13:38:02 +0530

    Removing the setting to change notification sound

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -54,15 +54,15 @@ class SettingsPage extends StatelessWidget {
                 );
               },
             ),
-            ListTile(
-              title: Text('Sound'),
-              subtitle: Text('Birds'),
-              trailing: Icon(Icons.chevron_right),
-              leading: Icon(Icons.music_note),
-              onTap: () {
-                //let user change theme of the app
-              },
-            )
+            // ListTile(
+            //   title: Text('Sound'),
+            //   subtitle: Text('Birds'),
+            //   trailing: Icon(Icons.chevron_right),
+            //   leading: Icon(Icons.music_note),
+            //   onTap: () {
+            //     //let user change theme of the app
+            //   },
+            // )
           ],
         ),
       ),
